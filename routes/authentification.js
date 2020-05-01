8a0069a8c0eef63864769b2dd28b28594123b48e
Kyleus <guillaume.thib02@gmail.com>
2020-05-01 11:39:57 -0500

    debug

--- routes/authentification.js
+++ routes/authentification.js
@@ -7,21 +7,21 @@ const User = require("../models/User.model");
 router.route("/login").post(async (req,res) => {
     let user = await User.findOne({email:req.body.email})
     if(!user){
-        res.status(403).json({error:"Wrong password or email"})
+        res.json({error:"Wrong password or email"})
     }
 
     if(bcrypt.compareSync(req.body.password, user.password)){
         let token = jwt.sign({email:user.email},"secret",{expiresIn: "2 days"})
         res.json({token})
     }else{
-        res.status(403).json({error:"Wrong password or email"})
+        res.json({error:"Wrong password or email"})
     }
 })
 
 router.route('/register').post(async (req,res) => {
     let user = await User.findOne({email:req.body.email})
     if(user){
-        res.status(403).json({error:"Email already use"});
+        res.json({error:"Email already use"});
     }
     if(req.body.email && req.body.password && req.body.firstName && req.body.lastName && req.body.age && req.body.sex && req.body.address){
         let hashedPassword = bcrypt.hashSync(req.body.password,8);
@@ -29,11 +29,11 @@ router.route('/register').post(async (req,res) => {
         let newUser = new User(req.body);
         newUser.password = hashedPassword;
         newUser.save()
-            .then(() => res.status(200).json(newUser))
-            .catch((err) => res.status(500).json({error:err}))
+            .then(() => res.json(newUser))
+            .catch((err) => res.json({error:err}))
 
     }else{
-        res.status(400).json({error:"Missing required field"})
+        res.json({error:"Missing required field"})
     }
 })
 

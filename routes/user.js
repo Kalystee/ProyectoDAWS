7e239b6bff5cf3d15985f04f1b3a59591fce54a2
Kyleus <guillaume.thib02@gmail.com>
2020-05-13 02:55:50 -0500

    fix service check

--- routes/user.js
+++ routes/user.js
@@ -3,21 +3,21 @@
 const router = require('express').Router()
 let User = require("../models/User.model")
 const auth = require("./middleware/auth")
-router.get("/",auth.checkToken,async (req,res) => {
+router.get("/",auth.checkToken,(req,res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json({error:err}));
 });
 
-router.route('/:email').get((req,res) => {
+router.get("/:email",auth.checkToken,(req,res) => {
     User.findOne({email:req.params.email})
         .then(user => res.json(user))
         .catch(err => res.status(400).json({error:err}));
-})
+});
 
 router.route('/:email').delete((req,res) => {
     User.findOneAndDelete({email:req.params.email})
-        .then(user => res.json())
+        .then(user => res.json(user))
         .catch(err => res.status(400).json({error:err}));
 })
 
